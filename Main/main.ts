9b5b72dc3fc13278f763e5cae82a15a7315e56cd
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-06 00:00:19 -0500

    working on ts project

--- Main/main.ts
+++ Main/main.ts
@@ -1,2 +1,8 @@
 // this is going to be the main file which would basically call the handling on the program that I'm trying to do. 
-//TODO implement a simple but also cool to do list adder from CLI into an array then push that array back out to the terminal with a cool terminal Interface. 
+//TODO implement a simple but also cool to do list adder from CLI into an array then push that array back out to the terminal with a cool terminal Interface.  
+// start with the import statements  
+require('../Functions'); // had to install npm i --save-dev @types/node 
+
+// imported the functions folder so it should use all the files inside that function folder. 
+// we can call in the main the entire set of functions in order to start the program off. 
+
